5992329cf47e70fba20b5375abdf15994f0088c8
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-22 10:35:19 +0200

    Initial commit

---
+++ src/main/java/Back_end/gameSlide/Capstone/Back_end/gameSlide/Capstone/BackEndGameSlideCapstoneApplication.java
@@ -0,0 +1,13 @@
+package Back_end.gameSlide.Capstone.Back_end.gameSlide.Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGameSlideCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndGameSlideCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Back_end/gameSlide/Capstone/Back_end/gameSlide/Capstone/BackEndGameSlideCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package Back_end.gameSlide.Capstone.Back_end.gameSlide.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndGameSlideCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
